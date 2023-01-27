d64b997e6ee823e2198e2d49f89e30cb50aca225
mertSoyunmezoglu <mert.soyunmezoglu@etiya.com>
2023-01-27 12:38:08 +0300

    ikinci commit

--- src/main/java/com/etiya/emojigame/repositories/UserRepository.java
+++ src/main/java/com/etiya/emojigame/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.etiya.emojigame.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 
